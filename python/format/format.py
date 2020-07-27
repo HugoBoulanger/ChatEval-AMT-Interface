f7da22a1df921098c8b2da355ba2bde06409d96a
Mathilde Veron <mathilde.veron@lne.fr>
2020-07-27 13:58:32 +0200

    fix bugs in format (duplicated and missing turns to annotate)

--- python/format/format.py
+++ python/format/format.py
@@ -30,12 +30,16 @@ def get_rows_per_dialog(rows):
             rows_per_dialog.append(d_current)
             d_current = [r]
             d_uid = d_uid_current
+    rows_per_dialog.append(d_current)
     return rows_per_dialog
 
 
 def format_validity(sce_path, dst_path, nb_annot_per_hit=3):
+    print(f'--- {sce_path} ---')
     sce_rows = read_csv(sce_path, with_header=True)
     rows_per_dialog = get_rows_per_dialog(sce_rows)
+    print(f'nb of dialogues = {len(rows_per_dialog)}')
+    print(f'nb of turns to annotate expected = {len(sce_rows) - len(rows_per_dialog)}')
 
     validity_rows = []
     for turns in rows_per_dialog:
@@ -45,22 +49,24 @@ def format_validity(sce_path, dst_path, nb_annot_per_hit=3):
             for j in range(min(i+1, 4)):
                 validity_row[-(1+j)] = turns[i-j][2]
             validity_rows.append(validity_row)
+    print(f'nb of turns to annotate = {len(validity_rows)}')
 
     headers = []
     for i in range(nb_annot_per_hit):
         headers.extend([f'NUMBER_{i+1}', f'DIALOG_{i+1}_1', f'DIALOG_{i+1}_2', f'DIALOG_{i+1}_3', f'DIALOG_{i+1}_4'])
     dst_rows = [headers]
-    # dst_rows = [['NUMBER_1', 'DIALOG_1_1', 'DIALOG_1_2', 'DIALOG_1_3', 'DIALOG_1_4',
-    #              'NUMBER_2', 'DIALOG_2_1', 'DIALOG_2_2', 'DIALOG_2_3', 'DIALOG_2_4',
-    #              'NUMBER_3', 'DIALOG_3_1', 'DIALOG_3_2', 'DIALOG_3_3', 'DIALOG_3_4']]
 
     random.shuffle(validity_rows)
     nb_hits = len(validity_rows) // nb_annot_per_hit + (1 if len(validity_rows) % nb_annot_per_hit != 0 else 0)
+    print(f'nb of hits expected = {nb_hits}')
     for i in range(nb_hits):
+        j_min = i * nb_annot_per_hit
+        j_max = min(len(validity_rows), j_min + nb_annot_per_hit)
         dst_row = []
-        for j in range(i, min(len(validity_rows), i+3)):
+        for j in range(j_min, j_max):
             dst_row.extend(validity_rows[j])
         dst_rows.append(dst_row)
+    print(f'nb of hits = {len(dst_rows) - 1}')
 
     write_csv(dst_path, dst_rows)
 
@@ -69,6 +75,11 @@ if __name__ == '__main__':
     random.seed(1)
 
     dir = os.path.dirname(os.path.abspath(__file__))
+
     sce = os.path.join(dir, 'topical_sample.csv')
     dst = os.path.join(dir, 'topical_sample_amt_validity.csv')
+    format_validity(sce, dst)
+
+    sce = os.path.join(dir, 'empathy_sample.csv')
+    dst = os.path.join(dir, 'empathy_sample_amt_validity.csv')
     format_validity(sce, dst)
