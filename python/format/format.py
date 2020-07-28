5e389cf5c9052fda608fab5592557fabcd57679c
Mathilde Veron <mathilde.veron@lne.fr>
2020-07-28 19:06:08 +0200

    pad csv file for AMT with attention check with random turns

--- python/format/format.py
+++ python/format/format.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import random
+import numpy as np
 
 
 def read_csv(path, with_header=False):
@@ -34,54 +35,80 @@ def get_rows_per_dialog(rows):
     return rows_per_dialog
 
 
-def format_validity(sce_path, dst_path, nb_annot_per_hit=3):
+def get_dialogue_segments(rows_per_dialog, nb_context_turn):
+    dialogue_segments = []
+    for turns in rows_per_dialog:
+        for i in range(1, len(turns)):
+            segment = [turns[i][0]] + ['[empty line]'] * nb_context_turn
+            for j in range(min(i+1, nb_context_turn)):
+                segment[-(1+j)] = turns[i-j][2]
+            dialogue_segments.append(segment)
+    return dialogue_segments
+
+
+def get_attention_check(rows_per_dialog, nb_context_turn):
+    indexes = random.sample(list(range(len(rows_per_dialog))), 2)
+    t1 = rows_per_dialog[indexes[0]][random.randint(0, len(rows_per_dialog[indexes[0]]) - 1)][2]
+    t2 = rows_per_dialog[indexes[1]][random.randint(0, len(rows_per_dialog[indexes[1]]) - 1)][2]
+    return ['[attention check]'] + ['[empty line]'] * (nb_context_turn - 2) + [t1, t2]
+
+
+def format_validity(sce_path, dst_dir_path, nb_context_turn=4, nb_annot_per_hit=3, attention_check='default'):
     print(f'--- {sce_path} ---')
     sce_rows = read_csv(sce_path, with_header=True)
     rows_per_dialog = get_rows_per_dialog(sce_rows)
     print(f'nb of dialogues = {len(rows_per_dialog)}')
-    print(f'nb of turns to annotate expected = {len(sce_rows) - len(rows_per_dialog)}')
+    # print(f'nb of turns to annotate expected = {len(sce_rows) - len(rows_per_dialog)}')
 
-    validity_rows = []
-    for turns in rows_per_dialog:
-        for i in range(1, len(turns)):
-            validity_row = [''] + ['[empty line]'] * 4
-            validity_row[0] = turns[i][0]
-            for j in range(min(i+1, 4)):
-                validity_row[-(1+j)] = turns[i-j][2]
-            validity_rows.append(validity_row)
+    validity_rows = get_dialogue_segments(rows_per_dialog, nb_context_turn)
     print(f'nb of turns to annotate = {len(validity_rows)}')
 
     headers = []
-    for i in range(nb_annot_per_hit):
-        headers.extend([f'NUMBER_{i+1}', f'DIALOG_{i+1}_1', f'DIALOG_{i+1}_2', f'DIALOG_{i+1}_3', f'DIALOG_{i+1}_4'])
+    for i in range(nb_annot_per_hit + (1 if attention_check == 'random_turns' else 0)):
+        headers.append(f'NUMBER_{i + 1}')
+        for j in range(nb_context_turn):
+            headers.append(f'DIALOG_{i + 1}_{j + 1}')
     dst_rows = [headers]
 
-    random.shuffle(validity_rows)
     nb_hits = len(validity_rows) // nb_annot_per_hit + (1 if len(validity_rows) % nb_annot_per_hit != 0 else 0)
-    print(f'nb of hits expected = {nb_hits}')
+    # print(f'nb of hits expected = {nb_hits}')
+    if attention_check == 'random_turns':
+        for i in range(nb_annot_per_hit - len(validity_rows) % nb_annot_per_hit):
+            validity_rows.append(get_attention_check(rows_per_dialog, nb_context_turn))
+        if len(validity_rows) % nb_annot_per_hit != 0:
+            return
+    random.shuffle(validity_rows)
     for i in range(nb_hits):
         j_min = i * nb_annot_per_hit
         j_max = min(len(validity_rows), j_min + nb_annot_per_hit)
         dst_row = []
         for j in range(j_min, j_max):
-            dst_row.extend(validity_rows[j])
-        for j in range(j_max, j_min + nb_annot_per_hit):
-            dst_row.extend(['None', '[empty line]', '[empty line]', '[empty line]', 'Please select valid.'])
-        dst_rows.append(dst_row)
+            dst_row.append(validity_rows[j])
+        if attention_check == 'default':
+            for j in range(j_max, j_min + nb_annot_per_hit):
+                dst_row.append(['None', '[empty line]', '[empty line]', '[empty line]', 'Please select valid.'])
+        if attention_check == 'random_turns':
+            dst_row.append(get_attention_check(rows_per_dialog, nb_context_turn))
+        random.shuffle(dst_row)
+        dst_row_flat = []
+        for segment in dst_row:
+            dst_row_flat.extend(segment)
+        dst_rows.append(dst_row_flat)
+
     print(f'nb of hits = {len(dst_rows) - 1}')
 
+    dst_path = os.path.join(dst_dir_path,
+                            os.path.basename(sce_path).replace('.csv', f'_amt_validity_context_{nb_context_turn}_nb_annot_{nb_annot_per_hit}.csv'))
     write_csv(dst_path, dst_rows)
 
 
 if __name__ == '__main__':
     random.seed(1)
 
-    dir = os.path.dirname(os.path.abspath(__file__))
+    dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'data')
 
     sce = os.path.join(dir, 'topical_sample.csv')
-    dst = os.path.join(dir, 'topical_sample_amt_validity.csv')
-    format_validity(sce, dst)
+    format_validity(sce, dir, nb_context_turn=4, nb_annot_per_hit=10, attention_check='random_turns')
 
     sce = os.path.join(dir, 'empathy_sample.csv')
-    dst = os.path.join(dir, 'empathy_sample_amt_validity.csv')
-    format_validity(sce, dst)
+    format_validity(sce, dir, nb_context_turn=4, nb_annot_per_hit=10, attention_check='random_turns')
